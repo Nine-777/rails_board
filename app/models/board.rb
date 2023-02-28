cdbfff26a208bc7f4c04b09e1a023b0958478a94
KenHaya <hkearth777@gmail.com>
2023-02-28 21:07:16 +0900

    feat: annotate実装

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,16 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  title      :string(255)
+#  body       :text(65535)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Board < ApplicationRecord
+    has_many :comments
     validates :name, presence: true, length: { maximum: 10 }
     validates :name, presence: true, length: { maximum: 30 }
     validates :name, presence: true, length: { maximum: 1000 }
